fd42c7b9124d5060b0342ad42b432fb85d408c17
nzizapacifique250 <paccynziza@gmail.com>
2024-09-28 15:39:29 +0200

    Fixed some errors regarding pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,6 +7,7 @@ import console
 import inspect
 import pep8
 import unittest
+
 HBNBCommand = console.HBNBCommand
 
 
@@ -15,20 +16,25 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
+
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['console.py'])
+
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
+
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_console.py'])
+
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
+
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
@@ -36,7 +42,11 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
+
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
+        
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+        
+        
